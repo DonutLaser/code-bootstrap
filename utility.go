ee6603869694ccf107b9328f4b1d4a42f080b2e3
DonutLaser <vidmantas.luneckas@gmail.com>
2022-07-05 17:02:39 +0300

    chore: remove unused function

--- utility.go
+++ utility.go
@@ -1,17 +1,10 @@
 package main
 
 import (
-	"log"
 	"os"
 	"path"
 	"strings"
 )
-
-func checkError(err error) {
-	if err != nil {
-		log.Fatal(err)
-	}
-}
 
 func getExecutableDir() string {
 	exePath, _ := os.Executable()
