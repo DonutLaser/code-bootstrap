b67a77f31b2ac7f366fbd2a4f255a0f9280064bc
DonutLaser <vidmantas.luneckas@gmail.com>
2022-07-05 17:01:15 +0300

    settings: add method to update settings programatically

--- settings.go
+++ settings.go
@@ -29,7 +29,7 @@ func GetSettings() (Settings, bool) {
 			continue
 		}
 
-		key, value := getKeyValue(strings.TrimSpace(line))
+		key, value := getKeyValue(strings.TrimSpace(line), " ")
 
 		if key == "create_vscode_workspace" {
 			result.CreateVSCodeWorkspace = value == "true"
@@ -44,7 +44,30 @@ func GetSettings() (Settings, bool) {
 	return result, true
 }
 
-func getKeyValue(str string) (key string, value string) {
-	split := strings.Split(str, " ")
+func UpdateSettings(settings Settings, key string, value string) {
+	if key == "create_vscode_workspace" {
+		if !validateBool(value) {
+			fmt.Printf("`%s` only supports values `true` and `false`, but received value is `%s`", key, value)
+			return
+		}
+
+		settings.CreateVSCodeWorkspace = value == "true"
+	} else if key == "vscode_workspace_dir" {
+		settings.VSCodeWorkspaceDir = value
+	}
+
+	var sb strings.Builder
+	sb.WriteString(fmt.Sprintf("create_vscode_workspace %t\n", settings.CreateVSCodeWorkspace))
+	sb.WriteString(fmt.Sprintf("vscode_workspace_dir %s\n", settings.VSCodeWorkspaceDir))
+
+	WriteFile(fmt.Sprintf("%s/settings.conf", getExecutableDir()), sb.String())
+}
+
+func getKeyValue(str string, sep string) (key string, value string) {
+	split := strings.Split(str, sep)
 	return split[0], split[1]
+}
+
+func validateBool(value string) bool {
+	return value == "true" || value == "false"
 }
